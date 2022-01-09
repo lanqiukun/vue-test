2e8bc284055cfcf62e12851406f006832f72305f
lanqiukun <1543323033@qq.com>
2022-01-09 22:01:07 +0800

    asfd

---
+++ src/components/Article.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="to_article(12)">我要去看id为12的文章</button>
+    <button @click="to_article(20)">我要去看id为20的文章</button>
+    <button @click="to_about(20)">我要去看关于页面</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    to_article(id) {
+      this.$router.replace({ name: 'Article', params: { id }}).catch(error => {
+        // 避免导航到相同的路径报错，比如从 /article/12 跳到 /article/12 
+        console.log(error)
+      })
+    },
+    to_about() {
+      this.$router.replace({ name: 'About'})
+    }
+  },
+  activated() {
+    console.log('activated')
+  },
+  deactivated() {
+    console.log('deactivated')
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="to_article">我要去看看文章</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    to_article() {
+      this.$router.replace({ name: 'Article', params: { id: 12 }})
+    }
+  }
+}
+</script>
